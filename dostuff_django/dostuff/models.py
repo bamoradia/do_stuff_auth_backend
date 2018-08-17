d7b2af2882eb1a01075204cfc2871be8df5e287d
Steve <sbrennison@gmail.com>
2018-08-17 11:41:56 -0500

    created models

--- dostuff_django/dostuff/models.py
+++ dostuff_django/dostuff/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+  username = models.CharField(max_length=32)
+  password = models.CharField(max_length=32)
+  location = models.CharField(max_length=64)
+
+  def __str__(self):
+    return self.name
+
+class Event(models.Model):
+  name = models.CharField(max_length=100)
+  date = models.IntegerField()
+  time = models.CharField(max_length=32)
+  description = models.TextField()
+  url = models.TextField()
+
+  def __str__(self):
+    return self.name
+
+class Category(models.Model):
+  name = models.CharField(max_length=32)
+
+  def __str__(self):
+    return self.name
