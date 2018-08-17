cac32477c7a59df156a657de1ba3083d06133666
Steve <sbrennison@gmail.com>
2018-08-17 14:51:29 -0500

    cloning

--- dostuff_django/dostuff/views.py
+++ dostuff_django/dostuff/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import User, Event, Category, UserEvent, UserCategory, EventCategory
 
 # Create your views here.
+
+class
