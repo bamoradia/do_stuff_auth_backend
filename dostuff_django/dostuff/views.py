d19f3115d6487110f2cb4a84fe319581e1d90aea
Badal Moradia <badal@i360llc.com>
2018-08-19 17:47:25 -0500

    yelp api integrated to events get route

--- dostuff_django/dostuff/views.py
+++ dostuff_django/dostuff/views.py
@@ -7,6 +7,9 @@ from .serializers import EventSerializer
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
+import requests
+import time
+import datetime
 # import json
 
 
@@ -24,7 +27,42 @@ from django.contrib.auth.models import User
 
 def events_list(request):
 	events = Event.objects.all()
+	#do this after we final database grab of all events
 	events_serialized = serializers.serialize('json', events)
+
+	# Need to find user's location for intial event search
+
+	response = requests.get("https://api.yelp.com/v3/events?location=Chicago&limit=50&start_date=1534611850", headers={'Authorization': 'Bearer gr0amugCLWzgKkSCIgPZnPI8e7cRXFuEprIOGszYzUIo9JH5kWT1LMMZUkIW0tOBpywUrjmxns-zKDh5FoGsj4_SPNZG_-WDeGAzOCESd0wG9ZX5tUOXIRo4H2poW3Yx'})
+
+	response_json = response.json()
+
+	in_database = False
+
+	for i in range(0, length(response_json['events'])):
+		for j in range(0, length(events)):
+			if events[j]['url'] == response_json['event_site_url']:
+				in_database = True
+
+		if in_database == False:
+			#add event to database
+			date, time = response_json['time_start'].split(' ')
+			year, month, day = date.split('-')
+
+			s = '{}/{}/{}'.fomat(day, month, year)
+
+			unix_time = time.mktime(datetime.datetime.strptime(s, "%d/%m/%Y").timetuple())
+
+			e = Event(name=response_json['name'], date=unix_time, time=time, description=response_json['description'], url=response_json['event_site_url'])
+
+			events.append(e)
+			in_database = False
+
+
+	
+	# response.json()
+	# response_json = serializers.serialize('json', response)
+
+	print(response_json['events'][0], 'this is the reponse')
 
 	# events = Event.objects.all()
 	# serializer_class = EventSerializer
@@ -80,7 +118,7 @@ def create_user(request):
 def edit_user(request):
 	if request.method == 'PUT':
 		# request.POST['userid']
-		user_match = User.objects.get(pk=7)
+		user_match = User.objects.get(pk=8)
 		user_profile = UserProfile.objects.get(user=user_match)
 		# print(user_profile.location, 'this is user profile location')
 		print(request.POST, 'this is the request location')
