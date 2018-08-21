4693a5bf6bcb9bddbe992f8cd3de7e52fd135bf5
Steve <sbrennison@gmail.com>
2018-08-21 10:11:33 -0500

    Installing things

--- dostuff_django/dostuff/urls.py
+++ dostuff_django/dostuff/urls.py
@@ -17,3 +17,5 @@ path('api/testing', views.testing, name='testing'),
   # path('api/event/', views.EventList.as_view(), name='event-list'),
   # path('api/event/<int:pk>', views.EventDetail.as_view(), name='event-detail'),
 ]
+
+## COMBINE URLS WITH LOCALHOST:8000 TO MAKE API CALLS!!!!!!!!!!!!
