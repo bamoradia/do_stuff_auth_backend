07590dc25f2b3beab42edec633a1c7b49811c57c
Badal Moradia <badal@i360llc.com>
2018-09-21 10:20:52 -0500

    made migrations on local database

--- dostuff_django/settings.py
+++ dostuff_django/settings.py
@@ -89,26 +89,26 @@ WSGI_APPLICATION = 'dostuff_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'dostuff',
-#         'USER': 'dostuffadmin',
-#         'PASSWORD': 'funky-ducks',
-#         'HOST': 'localhost'
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'deofs2svc02ou4',
-        'USER': 'ltheljxsyzxsrd',
-        'PASSWORD': '7f24e9b60f12955ad1969e5284ed2f2755cadf7aba4bab763c745c4a7e82dd32',
-        'HOST': 'ec2-54-163-245-44.compute-1.amazonaws.com',
-        'PORT': 5432
+        'NAME': 'dostuff',
+        'USER': 'dostuffadmin',
+        'PASSWORD': 'funky-ducks',
+        'HOST': 'localhost'
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'deofs2svc02ou4',
+#         'USER': 'ltheljxsyzxsrd',
+#         'PASSWORD': '7f24e9b60f12955ad1969e5284ed2f2755cadf7aba4bab763c745c4a7e82dd32',
+#         'HOST': 'ec2-54-163-245-44.compute-1.amazonaws.com',
+#         'PORT': 5432
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
