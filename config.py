e65e55f0b419fc7d084bd57f67829e3e6270a2ae
Badal Moradia <badal@i360llc.com>
2018-10-03 10:36:15 -0500

    moved config file

---
+++ config.py
@@ -0,0 +1,7 @@
+config = {
+	'account_sid': "ACfbf02ec44aa05b5acc501fd124f4ef4c",
+	'auth_token': '80c548e33b468c16cbf85258d7815e10',
+	'x-profile-secret': 'MGmKfVQDJFVZkuIT4wgkXTpu',
+	'from': '+17088808673',
+	'to': '+16306870821'
+}
